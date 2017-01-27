7886056a17d9dccb8bec0ccb9debca40c1d2ae70
Simeon H.K. fitch <fitch@astraea.io>
2017-01-27 13:00:41 -0500

    Incremental work on testing Databricks Avro conversion.

--- src/main/scala/astraea/demo/DatabricksAvro.scala
+++ src/main/scala/astraea/demo/DatabricksAvro.scala
@@ -1,14 +1,20 @@
 package astraea.demo
 
 import geotrellis.proj4.LatLng
-import geotrellis.raster.{ByteConstantTile, MultibandTile}
+import geotrellis.raster.{ByteConstantTile, IntConstantTile, MultibandTile}
 import geotrellis.spark.TemporalProjectedExtent
 import geotrellis.spark.io.avro.AvroRecordCodec
 import geotrellis.spark.io.avro.codecs.Implicits._
 import geotrellis.vector.Extent
-
 import com.databricks.spark.avro.SchemaConverters
 import java.time.ZonedDateTime
+
+import com.databricks.spark.avro.hack.SchemaConvertersBackdoor
+import org.apache.avro.generic.GenericRecord
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.catalyst.encoders.RowEncoder
+import org.apache.spark.sql.catalyst.expressions.GenericRow
+import org.apache.spark.sql.types.StructType
 
 object DatabricksAvro extends TemporalProjectedExtentCodec {
 
@@ -26,7 +32,7 @@ object DatabricksAvro extends TemporalProjectedExtentCodec {
 
     val target = (tpe, tile)
 
-    val codec = codecOf(target)
+    val codec: AvroRecordCodec[(TemporalProjectedExtent, MultibandTile)] = codecOf(target)
 
     println(s"codec.schema.toString(true): ${codec.schema.toString(true)}")
 
@@ -36,5 +42,48 @@ object DatabricksAvro extends TemporalProjectedExtentCodec {
 
     println(s"sqlType: $sqlType")
     println(s"sqlType.dataType.prettyJson: ${sqlType.dataType.prettyJson}")
+
+
+    def makeRowEncoder[T](codec: AvroRecordCodec[T]) = {
+      val sqlType = SchemaConverters.toSqlType(codec.schema)
+      val structType = sqlType.dataType match {
+        case st: StructType ⇒ st
+        case _ ⇒ throw new IllegalArgumentException(s"${sqlType.dataType} not a struct")
+      }
+
+      val avroEncoder: (T) ⇒ GenericRecord = codec.encode
+
+      val structTranslator = SchemaConvertersBackdoor.createConverterToSQL(codec.schema, structType)
+
+      val cast = (a: AnyRef) ⇒ a.asInstanceOf[GenericRow]
+
+      val rowEncoder = RowEncoder(structType)
+
+      avroEncoder andThen structTranslator andThen cast andThen rowEncoder.toRow
+    }
+
+    val encoder = makeRowEncoder(codec)
+
+    val at = encoder(target)
+    println("at: " + at)
+
+
+
+//    val spark = SparkSession.builder()
+//      .master("local[2]")
+//      .appName("AvroReadBenchmark")
+//      .getOrCreate()
+//
+//    import spark.implicits._
+//
+//    val sc = spark.sparkContext
+//
+//    val gtrdd = sc.makeRDD(Seq(target))
+//
+//    val gtds = gtrdd.map(encoder).toDF
+//
+//    gtds.show()
+//
+//    spark.stop()
   }
 }
