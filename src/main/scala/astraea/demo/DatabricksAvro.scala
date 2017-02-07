9e248c54891c573970ca9ae0a2222ac5e8e692be
Simeon H.K. fitch <fitch@astraea.io>
2017-02-07 11:27:11 -0500

    Incremental backup commit.

--- src/main/scala/astraea/demo/DatabricksAvro.scala
+++ src/main/scala/astraea/demo/DatabricksAvro.scala
@@ -1,24 +1,74 @@
 package astraea.demo
 
-import geotrellis.proj4.LatLng
-import geotrellis.raster.{ArrayMultibandTile, ByteArrayTile, ByteConstantTile, IntConstantTile, MultibandTile}
-import geotrellis.spark.TemporalProjectedExtent
-import geotrellis.spark.io.avro.AvroRecordCodec
-import geotrellis.spark.io.avro.codecs.Implicits._
-import geotrellis.vector.Extent
-import com.databricks.spark.avro.SchemaConverters
 import java.time.ZonedDateTime
 
+import com.databricks.spark.avro.SchemaConverters
 import com.databricks.spark.avro.hack.SchemaConvertersBackdoor
-import org.apache.avro.generic.GenericRecord
-import org.apache.spark.rdd.RDD
+import geotrellis.proj4.LatLng
+import geotrellis.raster.{ByteArrayTile, MultibandTile}
+import geotrellis.spark.TemporalProjectedExtent
+import geotrellis.spark.io.avro.AvroRecordCodec
+import geotrellis.vector.Extent
 import org.apache.spark.sql._
-import org.apache.spark.sql.functions._
+import org.apache.spark.sql.catalyst.InternalRow
+import org.apache.spark.sql.catalyst.encoders.ExpressionEncoder
+import org.apache.spark.sql.catalyst.expressions.codegen.{CodegenContext, CodegenFallback, ExprCode}
+import org.apache.spark.sql.catalyst.expressions.{BoundReference, Expression, GenericRow, LeafExpression, Literal, NonSQLExpression, UnaryExpression}
 import org.apache.spark.sql.types._
 
-object DatabricksAvro extends TemporalProjectedExtentCodec {
+import scala.reflect.ClassTag
+import scala.reflect.runtime.universe._
 
-  def codecOf[T: AvroRecordCodec](t: T) = implicitly[AvroRecordCodec[T]]
+object DatabricksAvro extends TemporalProjectedExtentCodec {
+  case class EncodeUsingAvro[T: ClassTag](child: BoundReference, schema: StructType, codec: AvroRecordCodec[T]) extends UnaryExpression with NonSQLExpression with CodegenFallback {
+    override def dataType: DataType = schema
+
+    override def nullable: Boolean = false
+
+    override def eval(input: InternalRow): Any = {
+      if (input.isNullAt(child.ordinal)) {
+        null
+      }
+      else {
+        val obj = input.get(child.ordinal, null).asInstanceOf[T]
+        val encoded = codec.encode(obj)
+        val converter = SchemaConvertersBackdoor.createConverterToSQL(codec.schema, schema)
+        InternalRow.fromSeq(converter(encoded).asInstanceOf[GenericRow].toSeq)
+      }
+    }
+  }
+
+  private def codecOf[T: AvroRecordCodec](t: T) = implicitly[AvroRecordCodec[T]]
+
+
+  object AvroDerivedEncoder {
+    def apply[T: AvroRecordCodec: ClassTag: TypeTag]: Encoder[T] = {
+
+      val codec = implicitly[AvroRecordCodec[T]]
+      val sparkSchema = {
+        val sqlType = SchemaConverters.toSqlType(codec.schema)
+        sqlType.dataType match {
+          case st: StructType ⇒ st
+          case _ ⇒ throw new IllegalArgumentException(s"${sqlType.dataType} not a struct")
+        }
+      }
+
+      sparkSchema.printTreeString()
+
+      // Assuming "flat" means all columns are primitive types.
+      val flat = !sparkSchema.fields.exists(_.dataType.isInstanceOf[StructType])
+      val clsTag = implicitly[ClassTag[T]]
+      val serializer: Seq[Expression] = {
+        val br = BoundReference(0, sparkSchema, false)
+        // need to figure out how to use the codec.encode method via expression
+
+        Seq(EncodeUsingAvro(br, sparkSchema, codec))
+
+      }
+      val deserializer: Expression =  Literal("dummy")
+      ExpressionEncoder[T](sparkSchema, flat, serializer, deserializer, clsTag)
+    }
+  }
 
   val testData = {
 
@@ -66,7 +116,7 @@ object DatabricksAvro extends TemporalProjectedExtentCodec {
 //    // val encoder = makeRowEncoder(codec)
 
     val spark = SparkSession.builder()
-      .master("local[2]")
+      .master("local[1]")
       .appName("avro2spark")
       //.config("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
       //.config("spark.kryo.registrator", classOf[KryoRegistrator].getName)
@@ -78,6 +128,13 @@ object DatabricksAvro extends TemporalProjectedExtentCodec {
     val sc = spark.sparkContext
 
     val gtrdd = sc.makeRDD(Seq(target))
+
+    implicit val crsEncoder = AvroDerivedEncoder[TemporalProjectedExtent]
+
+    val gtdf = gtrdd.map(_._1).toDS
+
+    gtdf.printSchema()
+    gtdf.show(false)
 
 //    val trans: RDD[Row] = gtrdd.map { p ⇒
 //      val encoder = makeRowEncoder(codec)
