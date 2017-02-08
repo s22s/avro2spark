3007e049858ad285cf14f027517761d4b461214f
Simeon H.K. fitch <fitch@astraea.io>
2017-02-08 14:22:58 -0500

    Working example using full TPE/MultibandTile pair.

--- src/main/scala/astraea/demo/AvroDerivedSparkEncoder.scala
+++ src/main/scala/astraea/demo/AvroDerivedSparkEncoder.scala
@@ -1,14 +1,14 @@
 package astraea.demo
 
 import com.databricks.spark.avro.SchemaConverters
+import com.databricks.spark.avro.hack.SchemaConvertersBackdoor
 import geotrellis.spark.io.avro.AvroRecordCodec
 import org.apache.avro.generic.GenericRecord
 import org.apache.spark.sql.Encoder
-import org.apache.spark.sql.catalyst.InternalRow
-import org.apache.spark.sql.catalyst.encoders.ExpressionEncoder
+import org.apache.spark.sql.catalyst.encoders.{ExpressionEncoder, RowEncoder}
 import org.apache.spark.sql.catalyst.expressions.codegen.CodegenFallback
-import org.apache.spark.sql.catalyst.expressions.{BaseGenericInternalRow, BoundReference, Literal, NonSQLExpression, UnaryExpression}
-import org.apache.spark.sql.types.{DataType, StructField, StructType}
+import org.apache.spark.sql.catalyst.expressions.{BoundReference, Expression, GenericRow, Literal, NonSQLExpression, UnaryExpression}
+import org.apache.spark.sql.types._
 
 import scala.reflect.ClassTag
 
@@ -20,80 +20,76 @@ import scala.reflect.ClassTag
  */
 object AvroDerivedSparkEncoder {
 
+  /** Main entry point for creating a Spark `Encoder` from an implicitly available `AvroRecordCodec`. */
   def apply[T: AvroRecordCodec: ClassTag]: Encoder[T] = {
 
     val codec = implicitly[AvroRecordCodec[T]]
 
-    val sparkSchema = {
-      val sqlType = SchemaConverters.toSqlType(codec.schema)
-      sqlType.dataType match {
+    // Convert Avro schema to Spark schema via Databricks library
+    val sparkSchema = SchemaConverters.toSqlType(codec.schema).dataType match {
         case st: StructType ⇒ st
-        case _ ⇒ throw new IllegalArgumentException(s"${sqlType.dataType} not a struct")
-      }
+        case dt ⇒ throw new IllegalArgumentException(s"$dt not a struct. Use built-in Spark `Encoder`s instead.")
     }
 
+    // Now wondering if `flat` might mean that a single column is expanded into multiple ones?
+    // ~~Assuming "flat" means all columns are primitive types. Not really sure.~~
+    val isFlat = sparkSchema.fields.length == 1 && !sparkSchema.fields.head.dataType.isInstanceOf[StructType]
+
+    // As of Spark 2.1.0, the `ExpressionEncoder` API is the only publicly accessible means of creating
+    // RDD => Dataframe converters. This means using the Catalyst `Expression` API, where an execution plan is
+    // defined in AST-like form.
     ExpressionEncoder[T](
       schema = sparkSchema,
-      // Now wondering if `flat` might mean that a single column is expanded into multiple ones?
-      // ~~Assuming "flat" means all columns are primitive types. Not really sure.~~
-      flat = sparkSchema.fields.length == 1 && !sparkSchema.fields.exists(_.dataType.isInstanceOf[StructType]),
+      flat = isFlat,
       serializer = {
         val br = BoundReference(0, sparkDataType[T], false)
-        sparkSchema.fields.map(f ⇒ EncodeUsingAvro(br, f, codec))
+        val asAvro = EncodeToAvro(br)
+        sparkSchema.fields.map(f ⇒ ExtractFromAvro(asAvro, f))
       },
-      deserializer = Literal(null),
+      deserializer = Literal(null), // TODO? or not necessary?
       clsTag = implicitly[ClassTag[T]])
   }
 
+  /** Intermediate expression to convert an incoming expression of type `T` to an Avro [[GenericRecord]]. */
+  case class EncodeToAvro[T: AvroRecordCodec: ClassTag](child: Expression)
+    extends UnaryExpression with NonSQLExpression with CodegenFallback {
 
-  class InternalAvroRow(avroRecord: GenericRecord, sparkType: DataType) extends BaseGenericInternalRow {
-    override protected def genericGet(ordinal: Int): Any = {
-      val value = avroRecord.get(ordinal)
-      value
+    override def dataType: DataType = sparkDataType[GenericRecord] // BinaryType
+
+    override def nullable: Boolean = false
+
+    override protected def nullSafeEval(input: Any): GenericRecord = {
+      val codec = implicitly[AvroRecordCodec[T]]
+      val obj = input.asInstanceOf[T]
+      codec.encode(obj)
     }
-
-    override def numFields: Int = sparkType match {
-      case struct: StructType ⇒ struct.fields.length
-      case _ ⇒ 1
-    }
-
-    override def copy(): InternalRow = this
   }
 
-  case class EncodeUsingAvro[T: ClassTag](
-    child: BoundReference, sparkSchema: StructField, codec: AvroRecordCodec[T])
+  /** Expression for pulling a specific field out of */
+  case class ExtractFromAvro(child: Expression, sparkSchema: StructField)
     extends UnaryExpression with NonSQLExpression with CodegenFallback {
+
+    override protected def nullSafeEval(input: Any): Any = {
+      val avroRecord = input.asInstanceOf[GenericRecord]
+
+      val fieldValue = avroRecord.get(sparkSchema.name)
+
+      dataType match {
+        case nestedSchema: StructType ⇒
+          val nestedRecord = fieldValue.asInstanceOf[GenericRecord]
+          val converter = SchemaConvertersBackdoor.createConverterToSQL(nestedRecord.getSchema, nestedSchema)
+          val genericRow = converter(nestedRecord).asInstanceOf[GenericRow]
+          val sqlRowlEncoder = RowEncoder(nestedSchema)
+          sqlRowlEncoder.toRow(genericRow)
+
+        case _ ⇒ fieldValue
+      }
+    }
 
     override def dataType: DataType = sparkSchema.dataType
 
     override def nullable: Boolean = false
 
-    override def eval(input: InternalRow): Any = {
-      if (input.isNullAt(child.ordinal)) {
-        null
-      }
-      else {
-        val obj = input.get(child.ordinal, null).asInstanceOf[T]
-        val avroRecord = codec.encode(obj)
-
-        dataType match {
-          case s: StructType ⇒ new InternalAvroRow(avroRecord.get(sparkSchema.name).asInstanceOf[GenericRecord], sparkSchema.dataType)
-          case o ⇒ avroRecord.get(sparkSchema.name)
-        }
-
-
-
-        //avro2spark(avroEncoded, sparkSchema)
-
-        //val converter = SchemaConvertersBackdoor.createConverterToSQL(codec.schema, sparkSchema)
-        //val genericRow = converter(avroEncoded).asInstanceOf[GenericRow]
-        //val sqlRowlEncoder = RowEncoder(sparkSchema)
-
-        // XXX Data corruption happening here in writing to `UnsafeRow`.
-        //sqlRowlEncoder.toRow(genericRow)
-
-      }
-    }
   }
 
   /** Hack to get spark to create an `ObjectType` for us. */
@@ -102,5 +98,4 @@ object AvroDerivedSparkEncoder {
     val clsTag = implicitly[ClassTag[T]]
     DslSymbol(Symbol(clsTag.toString())).obj(clsTag.runtimeClass).dataType
   }
-
 }
--- src/main/scala/astraea/demo/DatabricksAvro.scala
+++ src/main/scala/astraea/demo/DatabricksAvro.scala
@@ -1,35 +1,28 @@
 package astraea.demo
 
 import java.time.ZonedDateTime
+import java.util.concurrent.atomic.AtomicInteger
 
-import com.databricks.spark.avro.SchemaConverters
-import com.databricks.spark.avro.hack.SchemaConvertersBackdoor
 import geotrellis.proj4.LatLng
 import geotrellis.raster.{ByteArrayTile, MultibandTile}
 import geotrellis.spark.TemporalProjectedExtent
-import geotrellis.spark.io.avro.AvroRecordCodec
 import geotrellis.vector.Extent
 import org.apache.spark.sql._
-import org.apache.spark.sql.catalyst.InternalRow
-import org.apache.spark.sql.catalyst.encoders.{ExpressionEncoder, RowEncoder}
-import org.apache.spark.sql.catalyst.expressions.codegen.CodegenFallback
-import org.apache.spark.sql.catalyst.expressions.{BoundReference, GenericInternalRow, GenericRow, Literal, NonSQLExpression, UnaryExpression}
-import org.apache.spark.sql.types._
-
-import scala.reflect.ClassTag
-import scala.reflect.runtime.universe._
 
 object DatabricksAvro extends TemporalProjectedExtentCodec {
 
+  val counter = new AtomicInteger(0)
 
-  val testData = {
+  def testData: (TemporalProjectedExtent, MultibandTile) = {
 
-    val tpe = TemporalProjectedExtent(Extent(0.0, 0.0, 8.0, 10.0), LatLng, ZonedDateTime.now())
+    val i = counter.getAndIncrement()
+
+    val tpe = TemporalProjectedExtent(Extent(1.0 + i, 2.0 + i, 3.0 + i, 4.0 + i), LatLng, ZonedDateTime.now())
 
     val tile = MultibandTile(
-      ByteArrayTile((1 to 9).map(_ ⇒ 1.toByte).toArray, 3, 3),
-      ByteArrayTile((1 to 9).map(_ ⇒ 2.toByte).toArray, 3, 3),
-      ByteArrayTile((1 to 9).map(_ ⇒ 3.toByte).toArray, 3, 3)
+      ByteArrayTile((1 to 9).map(_ ⇒ (1+i).toByte).toArray, 3, 3),
+      ByteArrayTile((1 to 9).map(_ ⇒ (2+i).toByte).toArray, 3, 3),
+      ByteArrayTile((1 to 9).map(_ ⇒ (3+i).toByte).toArray, 3, 3)
     )
 
      (tpe, tile)
@@ -37,13 +30,9 @@ object DatabricksAvro extends TemporalProjectedExtentCodec {
 
   def main(args: Array[String]): Unit = {
 
-    val target = testData
-
     val spark = SparkSession.builder()
-      .master("local[1]")
+      .master("local")
       .appName("avro2spark")
-      //.config("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
-      //.config("spark.kryo.registrator", classOf[KryoRegistrator].getName)
       .getOrCreate()
 
     import spark.implicits._
@@ -51,26 +40,18 @@ object DatabricksAvro extends TemporalProjectedExtentCodec {
     @transient
     val sc = spark.sparkContext
 
-    val gtrdd = sc.makeRDD(Seq(target))
+    val gtrdd = sc.makeRDD(Seq.fill(3)(testData))
 
+    implicit val kvEncoder = AvroDerivedSparkEncoder[(TemporalProjectedExtent, MultibandTile)]
     implicit val tpeEncoder = AvroDerivedSparkEncoder[TemporalProjectedExtent]
-    implicit val extentEncoder = AvroDerivedSparkEncoder[Extent]
 
-    val gtdf = gtrdd.map(_._1).toDS
+    val gtdf = gtrdd.toDS
 
     gtdf.printSchema()
 
-    import org.apache.spark.sql.execution.debug._
-
     gtdf.show(false)
 
-//    val trans: RDD[Row] = gtrdd.map { p ⇒
-//      val encoder = makeRowEncoder(codec)
-//      encoder(p)
-//    }
-//
-//    val gtdf = spark.createDataFrame(trans , sqlType.dataType.asInstanceOf[StructType])
-//
+
 //    val tsum = udf((row: Array[Byte]) ⇒ row.sum)
 //    //val tilef = udf((t: ArrayMultibandTile) ⇒ t)
 //
